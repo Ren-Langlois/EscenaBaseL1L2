d2d86fd3c535be20452133c047aac14df3963bc3
AxelSalama <101335608+AxelSalama@users.noreply.github.com>
2022-09-15 18:44:22 -0300

    Ah se
    se

---
+++ Assets/Scripts/PuntosNet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuntosNet : MonoBehaviour
+{
+    int puntos;
+    public void OnTriggerEnter(Collider Other)
+    {
+        if (Other.tag == "Carro")
+            Destroy(gameObject);
+        puntos += 1;
+
+    }
+}
